762be5d8e8e254ad424fea5ca87779e0e07d4c0e
Allen <allenshamrock37@gmail.com>
2024-06-04 16:06:47 +0300

    {Feat}:Implemented the get & post Produce routes & produce created the model

---
+++ server/routes/produce.py
@@ -0,0 +1,34 @@
+from  flask_restful import Resource
+from flask import make_response,jsonify,request 
+from models import Produce,uuid
+from config import db
+
+class Produce(Resource):
+    def get(self):
+        produces = Produce.query.all()
+        produce_list = []
+        for produce in produces:
+            produce_data = {
+                'id': produce.id,
+                'farmer_id': produce.farmer_id,
+                'produce_type': produce.produce_type,
+                'quantity': produce.quantity,
+                'price': produce.price
+            }
+            produce_list.append(produce_data)
+
+        response = make_response(jsonify(produce_list), 200)
+        return response
+    
+    def post(self):
+        data = request.json
+
+        new_produce = Produce(
+            farmer_id = uuid.UUID(data['farmer_id']),
+            produce_type= data['produce_type'],
+            quantity = data['quantity'],
+            price = data['price']
+        )
+        
+        db.session.add(new_produce)
+        db.session.commit()
--- server/seed.py
+++ server/seed.py
@@ -1,57 +1,49 @@
-from models import User,Animal,Order,Transaction
-from config import app,db
+from models import User, Farmer, Animal, Order, Transaction, Produce
+from config import app, db
 from werkzeug.security import generate_password_hash
 from faker import Faker
-from sqlalchemy.exc import IntegrityError
 import random
 
 fake = Faker()
 
 with app.app_context():
-    # User.query.delete()
-    # Animal.query.delete()
-    # Order.query.delete()
-    # Transaction.query.delete()
     db.drop_all()
     db.create_all()
 
     def generate_test_users(count=8):
         for _ in range(count):
-            username = fake.user_name()                 
+            username = fake.user_name()
             user = User(
-                username = username,
-                email = f"{username}@gmail.com",
-                profile_picture = fake.image_url(),
-                password_hash=generate_password_hash('password'),                              
+                username=username,
+                email=f"{username}@gmail.com",
+                profile_picture=fake.image_url(),
+                password_hash=generate_password_hash('password'),
             )
             db.session.add(user)
         db.session.commit()
-        # Import necessary modules and models
 
-    def generate_test_famers(count=4):
-        # Create an admin user
+    def generate_test_farmers(count=4):
         for _ in range(count):
             username = fake.user_name()
-            
-            user = User(
+            user = Farmer(
                 username=username,
                 email=f'{username}@gmail.com',
                 password_hash=generate_password_hash('password'),
-                profile_picture ='https://cdn-icons-png.flaticon.com/512/3135/3135715.png',  
-                role ='farmer'  
+                profile_picture='https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+                role='farmer'
             )
             db.session.add(user)
-        db.session.commit()                    
+        db.session.commit()
 
     def generate_test_animals(count=4):
-        users = User.query.filter_by(role='farmer').all()
-        animal_types = ['cow', 'dog', 'cat']  # Corrected animal types list
-        breeds = ['rotwiler', 'G-s', 'Rfeed']  # Corrected breeds list
+        farmers = Farmer.query.all()
+        animal_types = ['cow', 'dog', 'cat']
+        breeds = ['rotwiler', 'G-s', 'Rfeed']
 
         for _ in range(count):
-            farmer = random.choice(users)
-            animal_type = random.choice(animal_types)  # Select a random animal type
-            breed = random.choice(breeds)  # Select a random breed
+            farmer = random.choice(farmers)
+            animal_type = random.choice(animal_types)
+            breed = random.choice(breeds)
 
             animal = Animal(
                 farmer_id=farmer.id,
@@ -69,40 +61,59 @@ with app.app_context():
     def generate_test_orders(count=10):
         users = User.query.filter_by(role='user').all()
         animals = Animal.query.all()
-        states = ['pending','accepted','rejected']
+        produces = Produce.query.all()
+        states = ['pending', 'accepted', 'rejected']
+
         for _ in range(count):
-            user =random.choice(users)
-            animal =random.choice(animals)
+            user = random.choice(users)
+            animal = random.choice(animals)
+            produce = random.choice(produces)
             state = random.choice(states)
             order = Order(
-                user_id = user.id,
-                animal_id = animal.id,
-                status = state
+                user_id=user.id,
+                animal_id=animal.id,
+                produce_id=produce.id,
+                status=state
             )
             db.session.add(order)
         db.session.commit()
 
-    def generate_test_transaction(count=2):        
+    def generate_test_transactions(count=2):
         users = User.query.filter_by(role='user').all()
         orders = Order.query.filter_by(status='accepted').all()
 
         for _ in range(count):
-            user =random.choice(users)                       
-            order =random.choice(orders)      
+            user = random.choice(users)
+            order = random.choice(orders)
             transaction = Transaction(
-                amount = fake.random_int(min=1000,max=500000),
-                quantity = fake.random_int(min=1,max=50),
-                user_id = user.id,                
-                order_id = order.id
+                amount=fake.random_int(min=1000, max=500000),
+                quantity=fake.random_int(min=1, max=50),
+                user_id=user.id,
+                order_id=order.id
             )
             db.session.add(transaction)
         db.session.commit()
 
+    def generate_test_produces(count=4):
+        farmers = Farmer.query.all()
+        produce_types = ['peas', 'rice', 'beans', 'sorghum']
 
-if __name__ == '__main__':
-    with app.app_context():
-        generate_test_users()
-        generate_test_famers()
-        generate_test_animals()
-        generate_test_orders()
-        generate_test_transaction()
+        for _ in range(count):
+            farmer = random.choice(farmers)
+            produce_type = random.choice(produce_types)
+
+            produce = Produce(
+                farmer_id=farmer.id,
+                produce_type=produce_type,
+                price=fake.random_int(min=10000, max=500000),
+                quantity=fake.random_int(min=1, max=50),
+            )
+            db.session.add(produce)
+        db.session.commit()
+
+    generate_test_users()
+    generate_test_farmers()
+    generate_test_animals()
+    generate_test_produces()
+    generate_test_orders()
+    generate_test_transactions()
