276f15138662faaaa57686323f5f8d180e63e203
Allen <allenshamrock37@gmail.com>
2024-06-04 18:26:46 +0300

    {Enh}:"Added the produce endpoints"

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from config import app,api
-from models import User,Animal,Order,Transaction
-from routes import Signup, Login, Logout, AnimalsRoutes,OrderRoutes,Transactions
+from models import User,Animal,Order,Transaction,Produce
+from routes import Signup, Login, Logout, AnimalsRoutes, OrderRoutes, Transactions,Produces
 
 api.add_resource(Signup,'/signup')
 api.add_resource(Login,'/login')
@@ -8,6 +8,7 @@ api.add_resource(Logout,'/logout')
 api.add_resource(Transactions,'/transactions')
 api.add_resource(AnimalsRoutes, '/animals', '/animals/<string:id>')
 api.add_resource(OrderRoutes, '/orders')
+api.add_resource(Produces, '/produces', '/produces/<string:id>')
 
 if __name__ == '__main__':
     app.run(port=5555,debug=True)
--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -2,3 +2,4 @@ from .user import Signup,Login,Logout
 from .animal import AnimalsRoutes
 from .order  import OrderRoutes
 from .transaction import Transactions
+from .produce import Produces
